09d3e668ec512a28b35691367b5461f5c549bfbd
Eduardo Zapardiel <51031801+eduzapa10@users.noreply.github.com>
2019-05-25 21:12:45 +0200

    Initial commit
    menu_main is the main program of the rover.
    modes host the different modes of the rover and the main menu itself. Right now it only has locomotion mode.

---
+++ menu_main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 18:04:40 2019
+
+@author: Edu
+"""
+
+import modes
+
+modes.menu()
+
+print('Bye!')
---
+++ modes.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 18:23:31 2019
+
+@author: Edu
+"""
+
+# raw_input() is used to define inputs in older versions of python, and is the
+# one that works on the RaspberryPi. In the computer use input().
+
+def menu():
+    # Main menu of the rover modes. 
+    print('Stand-by Mode')
+    choice = input()
+    if choice == 'locomotion': 
+        # Enters Locomotion mode
+        loco()
+    else:
+        # If invalid choice of mode
+        print('Invalid mode. Do you want to exit? y/n')
+        choice = input()
+        if choice == 'n': # Remain
+            menu()
+        elif choice == 'y': # Exit 
+            return
+        else: # Invalid value, exit program
+            print('Invalid choice. Exiting program')
+            return
+    
+def loco():
+    # Locomotion mode
+    print('Locomotion Mode')
+    move = True # True to stay in the mode False to return to the main menu
+    choices = ('w','a','s','d') # only four choices to move
+    while move == True:
+        choice = input() # Select movement
+        if choice in choices:
+            print(choice)
+        else:
+            print('Invalid choice! Exit locomotion mode? y/n')
+            choice = input()
+            if choice == 'y': # Exit locomotion mode
+                move = False
+            elif choice == 'n': # Remain in locomotion mode
+                move = True
+            else: # Invalid value, returns to main menu
+                print('Invalid choice. Returning to main menu')
+                move = False  
+    # When move = False we exit the loop end go back to the main menu
+    menu()
